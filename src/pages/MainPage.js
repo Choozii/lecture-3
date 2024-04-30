5e0b7761867b2f9c252e2c314d61d9a02db2853a
Choozii <jiyechoo@gmail.com>
2024-04-30 15:54:01 +0900

    perf: 이미지/폰트 최적화

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import BannerVideo from '../components/BannerVideo'
 import ThreeColumns from '../components/ThreeColumns'
 import TwoColumns from '../components/TwoColumns'
 import Card from '../components/Card'
 import Meta from '../components/Meta'
-import main1 from '../assets/main1.jpg'
-import main2 from '../assets/main2.jpg'
-import main3 from '../assets/main3.jpg'
-import main_items from '../assets/main-items.jpg'
-import main_parts from '../assets/main-parts.jpg'
-import main_styles from '../assets/main-styles.jpg'
+import main1 from '../assets/main1.webp'
+import main2 from '../assets/main2.webp'
+import main3 from '../assets/main3.webp'
+import main_items from '../assets/main-items.webp'
+import main_parts from '../assets/main-parts.webp'
+import main_styles from '../assets/main-styles.webp'
 
 function MainPage(props) {
+	const img1 = useRef(null);
+	const img2 = useRef(null);
+
+	useEffect(()=>{
+		const option = {};
+
+		const callback = (entries) => {
+			entries.forEach(entry => {
+				if(entry.isIntersecting){
+					console.log(entry.target)
+					entry.target.src = entry.target.dataset.src;
+					observer.unobserve(entry.target);
+				}
+			})
+		}
+		
+		const observer = new IntersectionObserver(callback, option);
+		observer.observe(img1.current);
+		observer.observe(img2.current);
+
+
+		return () => {
+			observer.disconnect();
+		}
+	},[])
+
 	return (
 		<div className="MainPage -mt-16">
 			<BannerVideo/>
@@ -42,14 +68,14 @@ function MainPage(props) {
 							content={'롱보드는 데크, 트럭, 휠, 킹핀, 베어링 등 여러 부품들로 구성됩니다. 롱보드를 타다보면 조금씩 고장나는 부품이 있기 마련인데, 이럴때를 위해 롱보들의 부품들에 대해서 알고 있으면 큰 도움이 됩니다.'}
 							btnLink={'/part'}
 						/>,
-						<img src={main_parts} />
+						<img data-src={main_parts} ref={img1}/>
 					]}
 					mobileReverse={true}
 				/>
 				<TwoColumns
 					bgColor={'#f4f4f4'}
 					columns={[
-						<img src={main_styles} />,
+						<img data-src={main_styles} ref={img2}/>,
 						<Meta
 							title={'Riding Styles'}
 							content={'롱보드 라이딩 스타일에는 크게 프리스타일, 다운힐, 프리라이딩, 댄싱이 있습니다. 보통 롱보드는 라이딩 스타일에 따라 데크의 모양이 조금씩 달라집니다. 많은 롱보드 매니아들이 각 쓰임새에 맞는 보드들을 소유하고 있습니다.'}
